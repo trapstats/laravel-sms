a7ae71a6449e93d4be96a530c42deb6d2c0f967a
Nicholas Dykhuizen <nicholas@trapstats.com>
2022-07-31 11:11:14 -0600

    Initial commit

---
+++ src/Transport/SnsTransport.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Trapstats\Sms\Transport;
+
+use Aws\Sns\Exception\SnsException;
+use Aws\Sns\SnsClient;
+use Trapstats\Sms\Contracts\TransportContract;
+use Trapstats\Sms\SentMessage;
+use Trapstats\Sms\Sms;
+
+class SnsTransport implements TransportContract
+{
+    /**
+     * Create a new Sns transport instance.
+     *
+     * @param  \Aws\Sns\SnsClient  $client
+     * @param  array  $options
+     */
+    public function __construct(
+        protected SnsClient $client,
+        protected array $options
+    ) {
+        //...
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function send(Sms $message): ?SentMessage
+    {
+        $from = $message->getFrom() ?: $this->options['from'];
+
+        foreach ($message->getTo() as $to) {
+            try {
+                $this->client->publish([
+                    'Message' => $message->getContent(),
+                    'PhoneNumber' => $to,
+                    'MessageAttributes' => [
+                        'AWS.SNS.SMS.SMSType' => [
+                            'DataType' => 'String',
+                            'StringValue' => 'Transactional' // $message->getDeliveryType(),
+                        ],
+                        'AWS.MM.SMS.OriginationNumber' => [
+                            'DataType' => 'String',
+                            'StringValue' => $from,
+                        ],
+                    ]
+                ]);
+            } catch (SnsException) {
+                return null;
+            }
+        }
+
+        return new SentMessage($message);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __toString(): string
+    {
+        return 'sns';
+    }
+}
